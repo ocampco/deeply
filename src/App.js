0cbede3bf3fd7f42963ae7c77374f057c063765e
ocampco <ocampco@gmail.com>
2023-05-27 16:51:27 +1000

    Fine tune splash screen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Splash from './components/Splash';
+import styles from './App.module.css';
 
-const App = () => (<Splash />);
+const App = () => (
+    <div className={styles.container}>
+        <Splash />
+    </div>
+);
 
 export default App;
--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Splash.module.css';
 
+const SplashContainer = ({ children }) => {
+    const [isVisible, setIsVisible] = useState(true);
+
+    useEffect(
+        () => { setTimeout(() => setIsVisible(false), '2500'); },
+        [],
+    );
+
+    return (isVisible && children)
+};
+
 const Splash = () => (
-    <div className={styles.container}>
+    <>
         <h1 className={styles.heading}>
             deeply
         </h1>
         <p className={styles.subheading}>
             meaningful questions, better connections
         </p>
-    </div>
+    </>
 );
 
-export default Splash;
+// export default SplashContainer;
+export default () => (
+    <SplashContainer><Splash /></SplashContainer>
+);
