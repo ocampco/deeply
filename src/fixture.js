2d4599e1fda1a5b056c220e42bd1288270169513
ocampco <ocampco@gmail.com>
2023-05-28 23:13:30 +1000

    Create mock fixture

--- src/fixture.js
+++ src/fixture.js
@@ -1,33 +1,46 @@
-// const intimate = [{
-//     question: 'What was a major landmark in your life that has permanently changed your perspective'
-// }];
+// TODO: Assign id, category, forGroup
+const casual = [
+    'spin a bottle. the person who it points to must describe how they think someone will be remembered. spin the bottle again to choose who.',
+    'what is a scenario you play out in your head often?',
+    'describe the person to your left\'s major eras in their life. periods that changed them. go around the circle.',
+    'name one thought that keeps you from sleeping at night.',
+    'name one thing you\'re grateful for that wakes you up in the morning.',
+    'name something small that you appreciate in life.',
+    'describe the person to your right\'s spirit animal. go around the circle.',
+    'on the count of 3, point to the person who you think would confess to the police',
+    'on the count of 3, hold up the number of fingers of people in the circle you think will be alive in 20 years.',
+    'on the count of 3, point to the person who would be the most generous if they won the lottery.',
+    'on the count of 3, hold up the number of fingers of people in the circle you think will be a completely different person in 3 years.',
+];
 
+const intimate = [
+    'what was a major landmark in your life that has permanently changed your perspective?',
+    'your life is a sail boat. describe the anchors holding you down.',
+    'your life is a sail boat. describe the wind keeping you sailing.',
+    'what do you think everyone\'s biggest achievement was from your perspective? go around the circle.',
+];
 
-// const questions = {
-//     intimate,
-// };
+const controversial = [
+    'what do you think is someone\'s biggest obstacle?',
+    'what do you think their biggest strength is to overcome their obstacle?',
+    'on the count of 3, everyone must point at someone in the circle. this person must describe something that their partner or close friends don\'t know they feel.',
+    'what was your last regret? go around the circle.',
+    'on the count of 3, raise your hand if you had one that got away. those with hands raised must talk about it.',
+    'on the count of 3, raise your hand if you agree with anything the world\'s biggest villains actions e.g. hitler, kim jong un. those with hands raised must talk about it.',
+    'on the count of 3, point to the person most likely to try something with a friend\'s partner',
+];
 
-// export default questions;
-// Intimate
-// sailboat analogy
+const questions = {
+    casual,
+    intimate,
+    controversial,
+};
 
-// what do you think xs biggest achievement has been
+export default questions;
 
-// Casual
-// bottle - how do you think x will be remembered
-// what is a scenario you play out in your head often
-// what are your eras ?
-// name 1 thing that keeps you up at night
-// name 1 thing that makes you wake up in the morning
-// something small that you appreciate in life
-// what do you think xs spirit animal is
+// TODO: 2 part questions
+// 'what do you think is someone\'s biggest obstacle?',
+// 'what do you think their biggest strength is to overcome their obstacle?',
 
-// Contraversial
-// what do you think x biggest obstacle
-// what do you think their biggest strength is to overcome their obstacle
-// point - something that your partner doesnt know you feel
-// what was your last regret
-// is there one that go away
-
-// Intimate / Contraversial
+// TODO: Multi category
 // when was the last time you cried
