71b08ea15310fe353ae433c2546878e866eeadfc
ocampco <ocampco@gmail.com>
2023-05-28 16:02:20 +1000

    Add mock fixture

---
+++ src/fixture.js
@@ -0,0 +1,33 @@
+// const intimate = [{
+//     question: 'What was a major landmark in your life that has permanently changed your perspective'
+// }];
+
+
+// const questions = {
+//     intimate,
+// };
+
+// export default questions;
+// Intimate
+// sailboat analogy
+
+// what do you think xs biggest achievement has been
+
+// Casual
+// bottle - how do you think x will be remembered
+// what is a scenario you play out in your head often
+// what are your eras ?
+// name 1 thing that keeps you up at night
+// name 1 thing that makes you wake up in the morning
+// something small that you appreciate in life
+// what do you think xs spirit animal is
+
+// Contraversial
+// what do you think x biggest obstacle
+// what do you think their biggest strength is to overcome their obstacle
+// point - something that your partner doesnt know you feel
+// what was your last regret
+// is there one that go away
+
+// Intimate / Contraversial
+// when was the last time you cried
