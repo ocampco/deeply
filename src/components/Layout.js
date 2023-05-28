22a687ab8dbec1ab01ab1ceee69889adf2dd0f39
ocampco <ocampco@gmail.com>
2023-05-28 10:43:23 +1000

    Use shared layout component

---
+++ src/components/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Layout.module.css';
+
+const Layout = ({ children }) => (
+    <div className={styles.container}>
+        { children }
+    </div>
+);
+
+export default Layout;
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,15 +1,16 @@
-import App from '../App';
+import Layout from '../components/Layout';
+import Home from '../components/Home';
 import Difficulty from '../components/Difficulty';
 
-export const PATH_APP = '/';
+export const PATH_HOME = '/';
 export const PATH_DIFFICULTY = '/difficulty';
 
 const ROUTES = [{
-    path: PATH_APP,
-    element: <App />,
+    path: PATH_HOME,
+    element: <Layout><Home /></Layout>,
 }, {
     path: PATH_DIFFICULTY,
-    element: <Difficulty />,
+    element: <Layout><Difficulty /></Layout>,
 }];
 
 export default ROUTES;
