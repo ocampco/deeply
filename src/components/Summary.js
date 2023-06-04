598034e5577becf4f256226432001066cb94a195
ocampco <ocampco@gmail.com>
2023-06-04 14:34:40 +1000

    Create summary page for end of questions

---
+++ src/components/Summary.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+    PATH_DONATE,
+    PATH_DIFFICULTY,
+} from '../constants/routes';
+import styles from './Summary.module.css';
+
+const Summary = () => (
+    <>
+        <h1 className={styles.heading}>
+            thanks for playing
+        </h1>
+        <div className={styles.buttons}>
+            <div className={styles.donate}>
+                <a
+                    href={PATH_DONATE}
+                    title='buy me a coffee'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                    <h2 className={styles.title}>
+                        did you like the game?
+                    </h2>
+                    <p className={styles.description}>
+                        if you'd like to help me make this app better, you can <p className={styles.underline}>buy me a coffee</p>
+                    </p>
+                </a>
+            </div>
+            <div className={styles.restart}>
+                <Link to={PATH_DIFFICULTY}>
+                    <h2 className={styles.buttonText}>
+                        try another
+                    </h2>
+                </Link>
+            </div>
+        </div>
+    </>
+);
+
+export default Summary;
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -3,12 +3,15 @@ import Home from '../components/Home';
 import Difficulty from '../components/Difficulty';
 import Instructions from '../components/Instructions';
 import Question from '../components/Question';
+import Summary from '../components/Summary';
 
-export const PATH_HOME = '/';
 export const PATH_DIFFICULTY = '/difficulty';
 export const PATH_INSTRUCTIONS = '/instructions';
 export const PATH_QUESTION_SEGMENT = '/questions';
+export const PATH_SUMMARY = '/summary';
+export const PATH_DONATE = 'https://www.buymeacoffee.com/ocampco';
 
+const PATH_HOME = '/';
 const PATH_QUESTION = `${PATH_QUESTION_SEGMENT}/:difficulty`;
 
 const ROUTES = [{
@@ -23,6 +26,9 @@ const ROUTES = [{
 }, {
     path: PATH_QUESTION,
     element: <Layout isDark><Question /></Layout>,
+}, {
+    path: PATH_SUMMARY,
+    element: <Layout><Summary /></Layout>,
 }];
 
 export default ROUTES;
