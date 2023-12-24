cb6fc97bc75be3c7121791f43ac5095b26f23155
ocampco <ocampco@gmail.com>
2023-12-24 15:42:45 +0700

    Make summary style consistent

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -9,31 +9,31 @@ import styles from './Summary.module.css';
 
 const Summary = () => (
     <>
-        <h1>thanks for playing</h1>
         <div className={styles.actions}>
-            <div className={styles.donate}>
-                <h2 className={styles.title}>
-                    did you like the game?
-                </h2>
-                <p className={styles.description}>
-                    if you'd like to help me make this app better, you can
-                </p>
-                <a
-                    href={PATH_DONATE}
-                    title='buy me a coffee'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    <p className={styles.link}>
-                        buy me a coffee
-                    </p>
-                </a>
-            </div>
-            <div className={styles.restart}>
-                <Link to={`${PATH_DIFFICULTY}?${QUERY_SHOW_PWA}`}>
-                    try another
-                </Link>
-            </div>
+          <h1>
+            thanks for playing
+          </h1>
+          <h2 className={styles.title}>
+            did you like the game?
+          </h2>
+          <p className={styles.description}>
+            if you'd like to help me make this app better, you can
+          </p>
+          <a
+              href={PATH_DONATE}
+              title='buy me a coffee'
+              target='_blank'
+              rel='noopener noreferrer'
+          >
+            <p className={styles.link}>
+              buy me a coffee
+            </p>
+          </a>
+        </div>
+        <div className={styles.restart}>
+          <Link to={`${PATH_DIFFICULTY}?${QUERY_SHOW_PWA}`}>
+            try another
+          </Link>
         </div>
     </>
 );
