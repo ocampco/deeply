ac4946473a84396e5df96bfde6fb4d49420c09e4
ocampco <ocampco@gmail.com>
2023-12-24 16:28:54 +0700

    Implement category change for instructions

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { PATH_QUESTION_SEGMENT } from '../constants/paths';
+import { PATH_DIFFICULTY, PATH_QUESTION_SEGMENT } from '../constants/paths';
 import styles from './Instructions.module.css';
+
+// TODO: Use shared component with styled-components
+const DIFFICULTY_BUTTON_TEXT = '< change category';
+const DifficultyButton = () => (
+  <Link
+    to={PATH_DIFFICULTY}
+  >
+      {DIFFICULTY_BUTTON_TEXT}
+  </Link>
+);
 
 const Instructions = () => {
     const { state: { difficulty } } = useLocation();
 
     return (
         <>
+          <DifficultyButton />
           <div className={styles.instructions}>
             <h1>before you start</h1>
             <ul className={styles.items}>
