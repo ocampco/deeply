02e1b23c3b11330f35b3523817f8b9f361e57ed8
ocampco <ocampco@gmail.com>
2023-12-24 19:11:35 +0700

    Use shared back link for instructions

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import BackLink from './BackLink';
 import { PATH_DIFFICULTY, PATH_QUESTION_SEGMENT } from '../constants/paths';
 import styles from './Instructions.module.css';
 
-// TODO: Use shared component with styled-components
-const DIFFICULTY_BUTTON_TEXT = '< change category';
-const DifficultyButton = () => (
-  <Link
-    to={PATH_DIFFICULTY}
-  >
-      {DIFFICULTY_BUTTON_TEXT}
-  </Link>
-);
+const DIFFICULTY_BUTTON_TEXT = 'change category';
 
 const Instructions = () => {
     const { state: { difficulty } } = useLocation();
 
     return (
         <>
-          <DifficultyButton />
+          <BackLink
+            path={PATH_DIFFICULTY}
+            text={DIFFICULTY_BUTTON_TEXT}
+          />
           <div className={styles.instructions}>
             <h1>before you start</h1>
             <ul className={styles.items}>
