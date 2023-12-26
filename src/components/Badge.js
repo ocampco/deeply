287b475deaf84a71903cc2e95bb8f547f9903c57
ocampco <ocampco@gmail.com>
2023-12-26 10:58:45 +0700

    Implement badge theming and use styled components

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import styles from './Badge.module.css';
+import styled from 'styled-components';
 
-const Badge = ({ children }) => (
-  <div className={styles.badge}>{children}</div>
+const Container = styled.div`
+  width: fit-content;
+  padding: 0.25rem 0.5rem;
+  margin-bottom: 0.25rem;
+  font-size: 1rem;
+  color: ${ props => props.secondary ? 'var(--default-primary-brown)' : 'var(--default-primary-beige)'};
+  background-color: ${ props => props.secondary ? 'var(--default-primary-beige)' : 'var(--default-primary-brown)'};
+  border-radius: var(--default-border-radius);
+  text-transform: uppercase;
+`;
+
+const Badge = ({ secondary, children }) => (
+  <Container secondary={secondary}>{children}</Container>
 );
 
 export default Badge;
