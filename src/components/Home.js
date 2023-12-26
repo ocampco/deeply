4d1d91ca3bc9e2ac08f732d2fe510c69504e4672
ocampco <ocampco@gmail.com>
2023-12-26 11:20:36 +0700

    Implement early access badge for home and refactor

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Badge from './Badge';
 import { PATH_DIFFICULTY } from '../constants/paths';
+// TODO: Migrate all to styled components
 import styles from './Home.module.css';
+
+const TEXT_HEADING = 'deeply';
+const TEXT_SUBHEADING = 'meaningful questions, better connections';
+const TEXT_BADGE = 'early access';
+const TEXT_BUTTON = 'start';
 
 const Home = () => (
     <>
-        <h1 className={styles.heading}>
-            deeply
-        </h1>
-        <h2 className={styles.subheading}>
-            meaningful questions, better connections
-        </h2>
+        <h1 className={styles.heading}>{TEXT_HEADING}</h1>
+        <h2 className={styles.subheading}>{TEXT_SUBHEADING}</h2>
+        <span className={styles.badge}>
+          <Badge>{TEXT_BADGE}</Badge>
+        </span>
         <div className={styles.link}>
-            <Link to={PATH_DIFFICULTY}>
-                start
-            </Link>
+          <Link to={PATH_DIFFICULTY}>
+            {TEXT_BUTTON}
+          </Link>
         </div>
     </>
 );
