14045e50b48082e45b5a2c1f36bb37b9b2189e09
ocampco <ocampco@gmail.com>
2023-12-28 20:19:34 +0700

    Convert to use styled components

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -4,13 +4,35 @@ import BackLink from './shared/BackLink';
 import Emoji from './shared/Emoji';
 import Button from './shared/Button';
 import { PATH_DIFFICULTY, PATH_QUESTION_SEGMENT } from '../constants/paths';
-import styles from './Instructions.module.css';
 
-const DIFFICULTY_BUTTON_TEXT = 'change category';
+const StyledContent = styled.div`
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const StyledItem = styled.li`
+  line-height: var(--default-line-height);
+  list-style: inside;
+  list-style-position: outside;
+  margin-left: 1.5rem;
+  margin-bottom: 0.5rem;
+
+  &:last-of-type {
+    margin-bottom: 0;
+  }
+`;
 
 const StyledButton = styled(Button)`
   margin: 0 auto;
 `;
+
+const TEXT_BUTTON_BACK = 'change category';
+const TEXT_BUTTON_NEXT = 'i\'m ready';
+const TEXT_HEADING = 'before you start ';
+const TEXT_ITEM_1 = 'please take your time to think about each answer';
+const TEXT_ITEM_2 = 'if you\'d like to drink, raise a glass and say "deep" if the answer resonates with you';
 
 const Instructions = () => {
     const { state: { difficulty } } = useLocation();
@@ -18,24 +40,23 @@ const Instructions = () => {
     return (
         <>
           <BackLink path={PATH_DIFFICULTY}>
-            {DIFFICULTY_BUTTON_TEXT}
+            {TEXT_BUTTON_BACK}
           </BackLink>
-          <div className={styles.content}>
-            <h1>before you start <Emoji label='raised hand'>✋</Emoji></h1>
+          <StyledContent>
+            <h1>
+              {TEXT_HEADING}
+              <Emoji label='raised hand'>✋</Emoji>
+            </h1>
             <ul>
-                <li className={styles.item}>
-                  please take your time to think about each answer
-                </li>
-                <li className={styles.item}>
-                  if you'd like to drink, raise a glass and say "deep" if the answer resonates with you
-                </li>
+              <StyledItem>{TEXT_ITEM_1}</StyledItem>
+              <StyledItem>{TEXT_ITEM_2}</StyledItem>
             </ul>
-          </div>
+          </StyledContent>
           <StyledButton
             to={`${PATH_QUESTION_SEGMENT}/${difficulty}`}
             animate
           >
-            i'm ready
+            {TEXT_BUTTON_NEXT}
           </StyledButton>
         </>
     )
