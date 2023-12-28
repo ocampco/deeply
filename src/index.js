9296ebe94dfb2a1fd4f0874c9c852f16ceed3099
ocampco <ocampco@gmail.com>
2023-12-28 14:54:40 +0700

    Properly use CSS files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import './styles/index.module.css';
+import './styles/index.css';
 import router from './router';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
