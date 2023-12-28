6155f721761df21b44af8dd0b0be6a7614ae8584
ocampco <ocampco@gmail.com>
2023-12-28 23:02:04 +0700

    Display last update time

--- src/components/CategoryContainer.js
+++ src/components/CategoryContainer.js
@@ -1,3 +1,6 @@
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Category, { PlaceholderCategory } from './Category';
 import categories from '../constants/categories';
@@ -14,29 +17,64 @@ const List = styled.ul`
   }
 `;
 
-const TEXT_HEADING = 'how deep would you like to go?';
+const Date = styled.p`
+  text-transform: lowercase;
+  margin-bottom: 2rem;
+`;
 
-const CategoryContainer = () => (
-  <>
-    <h1>{TEXT_HEADING}</h1>
-    <List>
-      <PlaceholderCategory />
-      { categories.map(({
-        url,
-        displayName,
-        emoji,
-        description,
-      }) => (
-        <Category
-          key={url}
-          url={url}
-          displayName={displayName}
-          emoji={emoji}
-          description={description}
-        />
-      ))}
-    </List>
-  </>
-);
+const TEXT_HEADING = 'how deep would you like to go?';
+const TEXT_DATE = 'last update: ';
+
+const parseDate = (dateString) => {
+  dayjs.extend(relativeTime)
+
+  return dayjs(dateString).fromNow();
+};
+
+// TODO: Migrate logic to API
+const CategoryContainer = () => {
+  const [updateDate, setUpdateDate] = useState();
+  const fetchUrl = 'https://api.github.com/repos/ocampco/deeply/commits/gh-pages';
+
+  useEffect(() => {
+    const fetchUpdateDate = async () => {
+      try {
+        const response = await fetch(fetchUrl);
+        const json = await response.json();
+        const date = parseDate(json.commit.author.date);
+
+        setUpdateDate(date);
+      } catch (error) {
+        console.log('Failed to fetch', error);
+      }
+    };
+
+    fetchUpdateDate();
+  }, []);
+
+  return (
+    <>
+      <h1>{TEXT_HEADING}</h1>
+      <Date>{TEXT_DATE}{updateDate}</Date>
+      <List>
+        <PlaceholderCategory />
+        { categories.map(({
+          url,
+          displayName,
+          emoji,
+          description,
+        }) => (
+          <Category
+            key={url}
+            url={url}
+            displayName={displayName}
+            emoji={emoji}
+            description={description}
+          />
+        ))}
+      </List>
+    </>
+  );
+};
 
 export default CategoryContainer;
