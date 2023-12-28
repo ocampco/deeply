d1a906d0ed0962cfd71c51fb8118ee2a6d4c3dfa
ocampco <ocampco@gmail.com>
2023-12-28 16:32:04 +0700

    Style placeholder level

--- src/components/Difficulty.js
+++ src/components/Difficulty.js
@@ -4,6 +4,15 @@ import difficultyLevels from '../constants/difficultyLevels';
 import { PATH_INSTRUCTIONS } from '../constants/paths';
 import styles from './Difficulty.module.css';
 
+// TODO: Clean up
+const PlaceholderDifficultyLevel = () => (
+  <li className={styles.placeholderLevel}>
+    <h2 className={styles.name}>
+      user submitted - coming soon <Emoji label='construction'>🚧</Emoji>
+    </h2>
+  </li>
+);
+
 // TODO: Add question path based on ID
 const DifficultyLevel = ({
     url,
@@ -27,35 +36,25 @@ const DifficultyLevel = ({
     </li>
 );
 
-// TODO: Clean up
-const PlaceholderDifficultyLevel = () => (
-  <li className={styles.level}>
-    <h2 className={styles.name}>
-      user submitted <Emoji label='construction'>🚧</Emoji>
-    </h2>
-    <p className={styles.description}>coming soon</p>
-  </li>
-);
-
 const Difficulty = () => (
     <>
         <h1>how deep would you like to go?</h1>
         <ul className={styles.levels}>
-        { difficultyLevels.map(({
-          url,
-          displayName,
-          emoji,
-          description,
-        }) => (
-          <DifficultyLevel
-              key={url}
-              url={url}
-              displayName={displayName}
-              emoji={emoji}
-              description={description}
-          />
-        ))}
-        <PlaceholderDifficultyLevel />
+          <PlaceholderDifficultyLevel />
+          { difficultyLevels.map(({
+            url,
+            displayName,
+            emoji,
+            description,
+          }) => (
+            <DifficultyLevel
+                key={url}
+                url={url}
+                displayName={displayName}
+                emoji={emoji}
+                description={description}
+            />
+          ))}
         </ul>
     </>
 );
