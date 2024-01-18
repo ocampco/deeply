a7e12a60baac41af455fe52e8cb95d6dbe659982
ocampco <ocampco@gmail.com>
2024-01-18 13:24:58 +0700

    Support secondary colour for button

--- src/components/shared/Button.js
+++ src/components/shared/Button.js
@@ -4,15 +4,21 @@ const Container = styled.div`
   border-radius: var(--default-border-radius);
   width: fit-content;
   height: fit-content;
-  background-color: var(--default-primary-white);
+  background-color: ${props => props.secondary
+    ? 'var(--default-primary-brown)'
+    : 'var(--default-primary-white)'
+  };
+  color: ${props => props.secondary
+    ? 'var(--default-primary-beige)'
+    : 'var(--default-primary-brown)'
+  };
   box-shadow: var(--default-box-shadow);
   padding: 1rem 2rem;
-  font-size: 1.125rem;
   font-weight: 700;
 `;
 
-const Button = ({ className, children }) => (
-  <Container className={className}>
+const Button = ({ className, secondary, children }) => (
+  <Container className={className} secondary={secondary}>
     {children}
   </Container>
 );
